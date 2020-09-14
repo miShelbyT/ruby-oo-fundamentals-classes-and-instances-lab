9d2ebec5dd6a9e198cfb5c91cb4f731d8d8dddca
miShelbyT <mi.shelbyrose@gmail.com>
2020-09-14 18:08:24 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+    # dogs are usually/always better than people
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+    #people are okay too. Dogs are better.
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
